e14bb7185f56e824a105172af59b026d35c3aee9
Bensmail-nrd <bensmail.nrd@gmail.com>
2022-08-18 18:31:59 +0100

    Initiation with Flask and get request

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+# import all neccessary  
 from flask import Flask, render_template
+from flask_restful import Api,Resource
+
+# launch the Flask app
 app = Flask(__name__)
 
+# launch the Api handler for the app
+api = Api(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Create a class that inherits from Resouce, just to handle and custamize the request type
+class Hello(Resource):
+    # override the get methode to handle its parameters
+    def get(self,name):
+        return {"data":"this is a response from your get request for you " + name}
+
+# add the resouce class to the api, then set a route for it
+api.add_resource(Hello,"/hellorequest/<string:name>")
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
